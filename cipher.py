f052275a9f8fc7c14717c046b248045a7b7874bb
BMourtzis <vasilis.mourtzis@outlook.com>
2015-10-20 23:56:57 +1100

    Added printing for both plain text and bits

--- cipher.py
+++ cipher.py
@@ -2,21 +2,24 @@
 # Filename: cipher.py
 from bitarray import bitarray #Import the BitArray Module (DO THIS ON ALL MACHINES)
 import pickle
+
 def main():
 	print("Welcome to group 12's cipher")
-	print("Please enter the path to the file you wish to Encrpyt")
-	fileName = raw_input()
+	#print("Please enter the path to the file you wish to Encrpyt")
+	#fileName = raw_input()
 
-	plainText=open(fileName,'r') #Takes the path given via user input and assigns the file to object "PlainText", in read mode.
-	
+	plainText=open("plain.txt",'r') #Takes the path given via user input and assigns the file to object "PlainText", in read mode.
+        text = plainText.read()
+        print(text)
+
 	#TODO: Remove as they are done
 	#Implement Serialisation
 	#Inverse method
 	#P-BOX
 	#S-BOX
 	#Bit-Shift
-	
+
 	textBits = bitarray()
-	textBits.frombytes(plainText.read(),16)
+	textBits.frombytes(text)
 	print(textBits)
 if __name__ == "__main__": main() #Defines the Main module as "Main" and not a library
