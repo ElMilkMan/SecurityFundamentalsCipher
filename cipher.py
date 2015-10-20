4e786ffd5870b8c54ac23eb85cab8ca011624f79
BMourtzis <vasilis.mourtzis@outlook.com>
2015-10-21 02:04:58 +1100

    Added inversion method to have as an example product cipher

--- cipher.py
+++ cipher.py
@@ -3,6 +3,19 @@
 from bitarray import bitarray #Import the BitArray Module (DO THIS ON ALL MACHINES)
 import pickle
 
+def inverse(bits):
+	bitlist = list(bits)
+	for i in range (0,16):
+		bit = bitlist[i]
+		if bit == "0":
+			bit = "1"
+		else:
+			bit = "0"
+		bitlist[i] = bit
+	inversedbits = ''.join(bitlist)
+	return inversedbits
+
+
 def main():
 	print("Welcome to group 12's cipher")
 	#print("Please enter the path to the file you wish to Encrpyt")
@@ -10,7 +23,7 @@ def main():
 
 	plainText=open("plain.txt",'r') #Takes the path given via user input and assigns the file to object "PlainText", in read mode.
         text = plainText.read()
-        print(text)
+        #print(text)
 
 	#TODO: Remove as they are done
 	#Implement Serialisation
@@ -20,6 +33,12 @@ def main():
 	#Bit-Shift
 
 	textBits = bitarray()
-	textBits.frombytes(text)
-	print(textBits)
+	textBits.frombytes("ab")
+	print(textBits.to01())
+	inversedbits = inverse(textBits.to01())
+	print(inversedbits)
+	#print(textBits.to01())
+
+
+
 if __name__ == "__main__": main() #Defines the Main module as "Main" and not a library
