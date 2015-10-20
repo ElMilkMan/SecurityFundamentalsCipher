002ee8e98ee487a8fad850c33c3cdb0d727049c8
Michael <mr.mikiie@gmail.com>
2015-10-20 20:17:05 +1100

    Sort of did something, then didn't.

--- cipher.py
+++ cipher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # Filename: cipher.py
-
+from bitarray import bitarray #Import the BitArray Module (DO THIS ON ALL MACHINES)
+import pickle
 def main():
 	print("Welcome to group 12's cipher")
 	print("Please enter the path to the file you wish to Encrpyt")
@@ -13,5 +14,9 @@ def main():
 	#Inverse method
 	#P-BOX
 	#S-BOX
-	#
+	#Bit-Shift
+	
+	textBits = bitarray()
+	textBits.frombytes(plainText.read(),16)
+	print(textBits)
 if __name__ == "__main__": main() #Defines the Main module as "Main" and not a library
