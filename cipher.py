df72a87fe37b1571a6762725eb0c94a3c9720a83
Michael Condon <mr.mikiie@gmail.com>
2015-10-20 17:04:46 +1100

    Added beginnings of file reading for cipher

---
+++ cipher.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Filename: cipher.py
+
+print("Welcome to group 12's cipher")
+print("Please enter the path to the file you wish to Encrpyt")
+fileName = raw_input()
+
+plainText=open(filename,'r')
+print f.read()
