478d8bbd9a35b5fed0e22b211db7f7b91cd6ba4b
Michael <mr.mikiie@gmail.com>
2015-10-20 19:07:03 +1100

    Fixed Main method, added to-do list

--- cipher.py
+++ cipher.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 # Filename: cipher.py
 
-print("Welcome to group 12's cipher")
-print("Please enter the path to the file you wish to Encrpyt")
-fileName = raw_input()
+def main():
+	print("Welcome to group 12's cipher")
+	print("Please enter the path to the file you wish to Encrpyt")
+	fileName = raw_input()
 
-plainText=open(fileName,'r')
-print plainText.read()
+	plainText=open(fileName,'r') #Takes the path given via user input and assigns the file to object "PlainText", in read mode.
+	
+	#TODO: Remove as they are done
+	#Implement Serialisation
+	#Inverse method
+	#P-BOX
+	#S-BOX
+	#
+if __name__ == "__main__": main() #Defines the Main module as "Main" and not a library
